ec7f3dd47d792d64934f8374792e259a3c8beda9
mohammad_mursalin <mursalinlamon@gmail.com>
2024-02-25 22:19:50 +0600

    accessing the methods

---
+++ UserAccess.java
@@ -0,0 +1,9 @@
+public class UserAccess {
+    
+    public static void main(String[] args) {
+        
+        BankingApplication user = new BankingApplication("Mursalin",123456);
+
+        user.showMenu();
+    }
+}
